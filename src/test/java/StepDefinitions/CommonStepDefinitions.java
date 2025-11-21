aa934ffe5f9e9cd6b3e4537e484f47fa6951816b
V <V@DESKTOP-J3J0SV0>
2025-11-21 20:06:53 +0530

    driver options

--- src/test/java/StepDefinitions/CommonStepDefinitions.java
+++ src/test/java/StepDefinitions/CommonStepDefinitions.java
@@ -1,11 +1,11 @@
 package StepDefinitions;
 
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import ExtentFactory.DriverExtent;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 
@@ -16,7 +16,14 @@ public class CommonStepDefinitions {
 	public void LaunchBrowser() {
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\V\\Downloads\\edgedriver_win64 (4)\\msedgedriver.exe");
 //		WebDriverManager.edgedriver().setup();
-		EdgeDriver driver = new EdgeDriver();
+		EdgeOptions options = new EdgeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--remote-allow-origins=*");
+		EdgeDriver driver = new EdgeDriver(options);
 //		DriverExtent.getInstance().setDriver(driver);
 		DriverExtent.setDriver(driver);
 		driver.manage().window().maximize(); 
