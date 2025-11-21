2c14765f707cf4fb3f134aab1f917f9d08df2233
V <V@DESKTOP-J3J0SV0>
2025-11-21 20:31:58 +0530

    updated driver args

--- src/test/java/StepDefinitions/CommonStepDefinitions.java
+++ src/test/java/StepDefinitions/CommonStepDefinitions.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.edge.EdgeOptions;
 import ExtentFactory.DriverExtent;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 
@@ -14,15 +15,15 @@ public class CommonStepDefinitions {
 	
 	@Before
 	public void LaunchBrowser() {
-		System.setProperty("webdriver.edge.driver", "C:\\Users\\V\\Downloads\\edgedriver_win64 (4)\\msedgedriver.exe");
-//		WebDriverManager.edgedriver().setup();
-		EdgeOptions options = new EdgeOptions();
-		options.addArguments("--disable-gpu");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--remote-allow-origins=*");
-		EdgeDriver driver = new EdgeDriver(options);
+//		System.setProperty("webdriver.edge.driver", "C:\\Users\\V\\Downloads\\edgedriver_win64 (4)\\msedgedriver.exe");
+		WebDriverManager.edgedriver().setup();
+//		EdgeOptions options = new EdgeOptions();
+//		options.addArguments("--disable-gpu");
+//		options.addArguments("--no-sandbox");
+//		options.addArguments("--disable-dev-shm-usage");
+//		options.addArguments("--disable-extensions");
+//		options.addArguments("--remote-allow-origins=*");
+		EdgeDriver driver = new EdgeDriver();
 //		DriverExtent.getInstance().setDriver(driver);
 		DriverExtent.setDriver(driver);
 		driver.manage().window().maximize(); 
