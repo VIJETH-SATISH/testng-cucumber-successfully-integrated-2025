eacc5fc65e1e23730786ef93f55163949b54c540
V <V>
2025-12-23 13:24:01 +0530

    trying dynamic workflow with github

--- src/test/java/StepDefinitions/CucumberTestRunner.java
+++ src/test/java/StepDefinitions/CucumberTestRunner.java
@@ -1,19 +1,19 @@
 package StepDefinitions;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
-import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.Cucumber;
 //import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 			plugin = {"html:target/cucumber-html-report"},
 			glue = {"StepDefinitions"},
 			features = {"src/test/resources/features"},
-			tags = "@Spicejet",
+//			tags = "@Spicejet",
 			monochrome = true,
 			dryRun = false
 
