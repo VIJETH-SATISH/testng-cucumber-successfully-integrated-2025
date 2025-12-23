75cb414468555c146b0d7d4109063af0e2985387
V <V>
2025-12-23 13:24:29 +0530

    dynamic work flow

---
+++ src/test/java/StepDefinitions/CucumberJunitTestRunner.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+			plugin = {"html:target/cucumber-html-report"},
+			glue = {"StepDefinitions"},
+			features = {"src/test/resources/features"},
+			tags = "@Spicejet",
+			monochrome = true,
+			dryRun = false
+
+					
+)
+
+public class CucumberJunitTestRunner {
+
+}
