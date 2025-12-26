0d19bcdf55fbe73aa27f43cda8b1942fca06df46
V <V>
2025-12-26 14:18:21 +0530

    secret logic for workflow added

--- src/test/java/PageObjects/AmazonLoginPageObjects.java
+++ src/test/java/PageObjects/AmazonLoginPageObjects.java
@@ -18,6 +18,10 @@ public class AmazonLoginPageObjects extends BasePage {
 	@FindBy(id="nav-link-accountList")
 	private WebElement Sign_In_Drop_Down_BTN;
 	
+	
+	@FindBy(xpath="//span[contains(text(),'Hello')]")
+	private WebElement Hello_BTN;
+	
 	@FindBy(id="ap_email_login")
 	private WebElement email_TXT;
 	
@@ -30,20 +34,38 @@ public class AmazonLoginPageObjects extends BasePage {
 	@FindBy(id="signInSubmit")
 	private WebElement signIn_BTN;
 	
+	@FindBy(id="user-name")
+	private WebElement userName_saucedemo;
+	
+	@FindBy(id="password")
+	private WebElement password_saucedemo;
+	
+	@FindBy(id="login-button")
+	private WebElement loginbutton_saucedemo;
 	
 	public void enterSearchItem() {	
 		
 		String amazon_User = System.getenv("AMAZON_USER_NAME");
 		String aws_Password = System.getProperty("aws.password");
 		
-		longWait.get().until(ExpectedConditions.elementToBeClickable(Sign_In_Drop_Down_BTN)).click();
-		longWait.get().until(ExpectedConditions.elementToBeClickable(email_TXT)).sendKeys("vijeth2011@gmail.com");
-		mediumWait.get().until(ExpectedConditions.elementToBeClickable(continue_BTN)).click();
-		longWait.get().until(ExpectedConditions.elementToBeClickable(pwd_TXT)).sendKeys("Vijuamazon@123");
-		mediumWait.get().until(ExpectedConditions.elementToBeClickable(signIn_BTN)).click();
+		/*****
+			longWait.get().until(ExpectedConditions.elementToBeClickable(Hello_BTN)).click();
+			longWait.get().until(ExpectedConditions.elementToBeClickable(email_TXT)).sendKeys(amazon_User);
+			mediumWait.get().until(ExpectedConditions.elementToBeClickable(continue_BTN)).click();
+			longWait.get().until(ExpectedConditions.elementToBeClickable(pwd_TXT)).sendKeys(aws_Password);
+			mediumWait.get().until(ExpectedConditions.elementToBeClickable(signIn_BTN)).click();		
+		****/
 		
+		//standard_user
+		//secret_sauce
+		longWait.get().until(ExpectedConditions.elementToBeClickable(userName_saucedemo)).sendKeys("standard_user");
+		longWait.get().until(ExpectedConditions.elementToBeClickable(password_saucedemo)).sendKeys("secret_sauce");
+		longWait.get().until(ExpectedConditions.elementToBeClickable(loginbutton_saucedemo)).click();
+		
+		/*
 		longWait.get().until(ExpectedConditions.visibilityOf(search_INPUT)).sendKeys("LEGO Speed Champions Ferrari");;
 		longWait.get().until(ExpectedConditions.elementToBeClickable(item_TXT)).click();
+		*/
 	}
 	
 	public void clickSearchItem() {
